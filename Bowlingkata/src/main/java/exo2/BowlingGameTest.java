3888c8a3a04f0e171f49ac09e3769c7f38b876a6
yassinezinabi <yasboy@hotmail.com>
2016-10-30 20:28:19 +0100

    utilisation de rollmany a la palce de boucle for dans la methode testallones

--- Bowlingkata/src/main/java/exo2/BowlingGameTest.java
+++ Bowlingkata/src/main/java/exo2/BowlingGameTest.java
@@ -23,8 +23,7 @@ public class BowlingGameTest extends TestCase {
 		  
 		  
 		  public void testAllOnes() throws Exception {
-		    for (int i = 0; i < 20; i++)
-		      g.roll(1);
+		    rollMany(20,1);
 		    assertEquals(20, g.score());
 		  }
 

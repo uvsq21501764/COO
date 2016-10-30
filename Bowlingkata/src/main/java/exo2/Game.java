a7e1dba3ea1927e9bfeef4581fb9f6410ae6159e
yassinezinabi <yasboy@hotmail.com>
2016-10-30 21:01:33 +0100

    changement de nom pour la variable i

--- Bowlingkata/src/main/java/exo2/Game.java
+++ Bowlingkata/src/main/java/exo2/Game.java
@@ -12,17 +12,22 @@ public class Game {
 
 	  public int score() {
 		    int score = 0;
-		    int i = 0;
-		    for (int frame = 0; frame < 10; frame++)  {
-		    	if (rolls[i] + rolls[i + 1] == 10) // spare
-		        {
-		          score += 10 + rolls[i + 2];
-		          i += 2;
-		        } else {
-		          score += rolls[i] + rolls[i + 1];
-		          i += 2;
-		        }
+		    int frameIndex = 0;
+		    for (int frame = 0; frame < 10; frame++) {
+		      if (rolls[frameIndex] + 
+		          rolls[frameIndex + 1] == 10) // spare
+		      {
+		        score += 10 + rolls[frameIndex + 2];
+		        frameIndex += 2;
+		      } else {
+		        score += rolls[frameIndex] + 
+		                 rolls[frameIndex + 1];
+		        frameIndex += 2;
+		      }
 		    }
+		   
+		    
+		    
 		    return score;
 		  }
 	}

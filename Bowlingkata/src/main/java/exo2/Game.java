12fd0d1e4b8deb244c76698aaaa30500abbb3459
yassinezinabi <yasboy@hotmail.com>
2016-10-30 20:09:52 +0100

    premier test erroné

--- Bowlingkata/src/main/java/exo2/Game.java
+++ Bowlingkata/src/main/java/exo2/Game.java
@@ -2,4 +2,14 @@ package exo2;
 
 public class Game {
 
+	public void roll(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object score() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }

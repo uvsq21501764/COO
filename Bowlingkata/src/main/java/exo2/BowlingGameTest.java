e6230c4d8ff8b5e2a09083c25bb5340309a3ad78
yassinezinabi <yasboy@hotmail.com>
2016-10-30 19:53:28 +0100

    premier commit et premier test reussi

---
+++ Bowlingkata/src/main/java/exo2/BowlingGameTest.java
@@ -0,0 +1,16 @@
+package exo2;
+
+import junit.framework.TestCase;
+
+public class BowlingGameTest extends TestCase {
+  public void testGutterGame() throws Exception {
+    Game g = new Game();
+  }
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

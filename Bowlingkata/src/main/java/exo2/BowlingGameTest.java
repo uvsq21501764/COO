f39270718911ad56c8b202f65feca7e92450f704
yassinezinabi <yasboy@hotmail.com>
2016-10-30 20:24:15 +0100

    ajout de la methode rollMany

--- Bowlingkata/src/main/java/exo2/BowlingGameTest.java
+++ Bowlingkata/src/main/java/exo2/BowlingGameTest.java
@@ -4,16 +4,26 @@ import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
 	 Game g = new Game();
+	 
+	 
 	 public void testGutterGame() throws Exception {
 		    int n = 20;
 		    int pins = 0;
-		    for (int i = 0; i < n; i++) {
-		      g.roll(pins);
-		    }
+		    rollMany(n, pins);
 		    assertEquals(0, g.score());
 		  }
+	 
+	 
 
+		  private void rollMany(int n, int pins) {
+		    for (int i = 0; i < n; i++)
+		      g.roll(pins);
+		  }
 
+		  
+		  
+		  
+		  
 		  public void testAllOnes() throws Exception {
 		    for (int i = 0; i < 20; i++)
 		      g.roll(1);

56be3c11fa388e3a03618947064bfb9d25463902
yassinezinabi <yasboy@hotmail.com>
2016-10-30 21:04:20 +0100

    ajout d'une methode pour verification du cas de spare retourne un boolean(isSpare())

--- Bowlingkata/src/main/java/exo2/Game.java
+++ Bowlingkata/src/main/java/exo2/Game.java
@@ -14,8 +14,7 @@ public class Game {
 		    int score = 0;
 		    int frameIndex = 0;
 		    for (int frame = 0; frame < 10; frame++) {
-		      if (rolls[frameIndex] + 
-		          rolls[frameIndex + 1] == 10) // spare
+		      if (isSpare(frameIndex)) // spare
 		      {
 		        score += 10 + rolls[frameIndex + 2];
 		        frameIndex += 2;
@@ -29,5 +28,12 @@ public class Game {
 		    
 		    
 		    return score;
+		  }
+	  
+	  
+	  
+	  private boolean isSpare(int frameIndex) {
+		    return rolls[frameIndex] +
+		           rolls[frameIndex + 1] == 10;
 		  }
 	}

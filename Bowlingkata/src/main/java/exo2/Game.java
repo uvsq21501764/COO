3ca5d9e88ee92cfa9e714b2808d0607c4cb86719
yassinezinabi <yasboy@hotmail.com>
2016-10-30 20:52:35 +0100

    oublié d'enlever l'initialisation score dans roll()

--- Bowlingkata/src/main/java/exo2/Game.java
+++ Bowlingkata/src/main/java/exo2/Game.java
@@ -1,12 +1,12 @@
 package exo2;
 
 public class Game {
-	  private int score = 0;
+	  
 	  private int rolls[] = new int[21];
 	  private int currentRoll = 0;
 
 	  public void roll(int pins) {
-	    score += pins;
+	
 	    rolls[currentRoll++] = pins;
 	  }
 
